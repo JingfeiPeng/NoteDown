50892574f467a4a754a8c2b01b42636e65071285
Ryan Larkin <ryan.larkin@uwaterloo.ca>
2022-02-09 00:49:05 -0500

    Add simple menus to UI

--- application/src/main/kotlin/Main.kt
+++ application/src/main/kotlin/Main.kt
@@ -1,20 +1,18 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
+
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.FrameWindowScope
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.compose.ui.Modifier
-
-import presentation.*
-
+import presentation.DocumentEditingArea
+import presentation.DocumentSelectionArea
+import presentation.MarkdownRendererArea
+import presentation.TextCustomizationMenu
 
 
 @Composable
@@ -43,8 +41,18 @@ fun MainArea() {
     }
 }
 
+@Composable
+fun FrameWindowScope.MenuItems()  {
+    MenuBar {
+        Menu("File") {}
+        Menu("Notes Calendar View") {}
+        Menu("Help") {}
+    }
+}
+
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
+        MenuItems()
         App()
     }
 }
