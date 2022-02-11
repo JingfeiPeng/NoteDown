8479c172b695db47f4dedd2441e9cb2d76463593
Ryan Larkin <ryan.larkin@uwaterloo.ca>
2022-02-11 11:36:13 -0500

    Give colour for selected folder and file

--- application/src/main/kotlin/presentation/DocumentSelectionArea.kt
+++ application/src/main/kotlin/presentation/DocumentSelectionArea.kt
@@ -2,11 +2,13 @@ package presentation
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import data.NoteFile
 import data.NoteFolder
 
@@ -15,13 +17,17 @@ fun DocumentSelectionArea(folders: List<NoteFolder>) {
     val selectedFolder = remember { mutableStateOf<NoteFolder?>(null) }
     val selectedFile = remember { mutableStateOf<NoteFile?>(null) }
 
+    val selectedColour = ButtonDefaults.buttonColors(backgroundColor = Color.Blue)
+    val unselectedColour = ButtonDefaults.buttonColors(backgroundColor = Color.Cyan)
+
     Row(Modifier.fillMaxSize()) {
         Box {
             Column {
                 folders.forEach {
+                    val colour = if (selectedFolder.value == it) selectedColour else unselectedColour
                     Button(onClick = {
                         selectedFolder.value = it
-                    }) {
+                    }, colors = colour) {
                         Text(it.name)
                     }
                 }
@@ -31,9 +37,10 @@ fun DocumentSelectionArea(folders: List<NoteFolder>) {
             selectedFolder.value?.let {
                 Column {
                     it.children.forEach {
+                        val colour = if (selectedFile.value == it) selectedColour else unselectedColour
                         Button(onClick = {
                             selectedFile.value = it
-                        }) {
+                        }, colors = colour) {
                             Text(it.name)
                         }
                     }
