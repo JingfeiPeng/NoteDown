19213eece25e1155f863fb234b326aebcc224847
Jingfei Peng <jf2peng@edu.uwaterloo.ca>
2022-02-16 01:34:10 -0500

    done

--- application/src/main/kotlin/persistence/FileIO.kt
+++ application/src/main/kotlin/persistence/FileIO.kt
@@ -2,12 +2,41 @@ package persistence
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.text.input.TextFieldValue
+import data.NoteFile
+import data.NoteFolder
 import java.io.File
 
-fun saveText(text: String) {
-    File("src/save.txt").writeText(text)
-}
 
-fun loadText(textState: MutableState<TextFieldValue>) {
-    textState.value = TextFieldValue(File("src/save.txt").readText())
+class FileIO {
+    companion object {
+        private const val notesFolder = "src/notes"
+
+        fun saveText(text: String, file: NoteFile?) {
+            if (file != null) {
+                file.file.writeText(text)
+            }
+        }
+
+        fun loadFile(textState: MutableState<TextFieldValue>, file: NoteFile) {
+            textState.value = TextFieldValue(file.file.readText())
+        }
+
+        fun readNotesFolder() : ArrayList<NoteFolder> {
+
+            val foldersAndFiles = ArrayList<NoteFolder>()
+
+            File(notesFolder).walk().forEach {
+                if (it.compareTo(File(notesFolder)) == 0) {
+                    return@forEach
+                }
+                if (it.isDirectory) {
+                    foldersAndFiles.add(NoteFolder(it))
+                } else {
+                    foldersAndFiles.last().addChild(NoteFile(it))
+                }
+            }
+
+            return foldersAndFiles
+        }
+    }
 }
