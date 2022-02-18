8210b2b8426f8e7e53601a80cddfac901fb0ab29
Jingfei Peng <jf2peng@edu.uwaterloo.ca>
2022-02-17 22:23:54 -0500

    notes

--- application/src/main/kotlin/presentation/DocumentEditingArea.kt
+++ application/src/main/kotlin/presentation/DocumentEditingArea.kt
@@ -1,6 +1,7 @@
 package presentation
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -16,16 +17,23 @@ fun DocumentEditingArea(
     selectedFile: MutableState<NoteFile?>,
     ) {
     Column {
-        TextField(
-            value = textState.value,
-            label = { Text("Jot something down...") },
-            onValueChange = {
-                textState.value = it
-                FileIO.saveText(textState.value.text.toString(), selectedFile.value)
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-        )
+        if (selectedFile.value == null) {
+            Text("Create a section and note to start editing",
+                style = MaterialTheme.typography.body2)
+        }
+        Column {
+            TextField(
+                value = textState.value,
+                enabled = selectedFile.value != null,
+                label = { Text("Jot something down...") },
+                onValueChange = {
+                    textState.value = it
+                    FileIO.saveText(textState.value.text.toString(), selectedFile.value)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            )
+        }
     }
 }
