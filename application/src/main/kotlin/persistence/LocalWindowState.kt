f9f78ee393f4537e6d2bb37a956c88b3867ba4a7
Aarsh Patel <aarsh937@gmail.com>
2022-03-25 01:54:30 -0400

    added themes

--- application/src/main/kotlin/persistence/LocalWindowState.kt
+++ application/src/main/kotlin/persistence/LocalWindowState.kt
@@ -1,5 +1,6 @@
 package persistence
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.window.WindowPosition
@@ -11,6 +12,7 @@ class LocalWindowState {
     private val savedReference = ".windowstate"
 
     val state: WindowState;
+    var colorTheme: Int = 0;
 
     constructor() {
         this.state = loadWindowState();
@@ -23,12 +25,13 @@ class LocalWindowState {
     private fun loadWindowState(): WindowState {
         if (this.isWindowStateStored()) {
             val storage: String = File(this.savedReferenceFolder, this.savedReference).readText(Charsets.UTF_8)
-            if (storage != null && storage.isNotEmpty() && storage.split(", ").size == 5) {
+            if (storage.isNotEmpty() && storage.split(", ").size == 6) {
                 val width: Float = storage.split(", ")[0].toFloat()
                 val height: Float = storage.split(", ")[1].toFloat()
                 val xpos: Float = storage.split(", ")[2].toFloat()
                 val ypos: Float = storage.split(", ")[3].toFloat()
                 val minimize: Boolean = storage.split(", ")[4].toBoolean()
+                this.colorTheme = storage.split(", ")[5].toInt()
                 return WindowState(
                     size = (DpSize(Dp(width), Dp(height))),
                     position = WindowPosition(Dp(xpos), Dp(ypos)),
@@ -39,12 +42,14 @@ class LocalWindowState {
         return WindowState()
     }
 
-    fun saveState() {
+    fun saveState(colorSelection: MutableState<Int>) {
         File(this.savedReferenceFolder, this.savedReference).writeText(
             this.state.size.width.value.toString()
-                + ", " + this.state.size.height.value.toString()
-                + ", " + this.state.position.x.value.toString()
-                + ", " + this.state.position.y.value.toString()
-                + ", " + this.state.isMinimized)
+                    + ", " + this.state.size.height.value.toString()
+                    + ", " + this.state.position.x.value.toString()
+                    + ", " + this.state.position.y.value.toString()
+                    + ", " + this.state.isMinimized
+                    + ", " + colorSelection.value.toString()
+        )
     }
 }
